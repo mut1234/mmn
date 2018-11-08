fd9a40b42e88389fb9c378ffc862326224c2a4cb
mut1234 <44782684+mut1234@users.noreply.github.com>
2018-11-08 22:20:57 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,18 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1).join(' ');
+    if (message.content.startsWith('$bc-bot')){
+    if(!message.author.id === '') return;
+    message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
+    client.users.forEach(m =>{
+    m.sendMessage(args)
+    })
+    }
+    });
+
+
+
+client.login("NDczNjMwNTg5MDU3MjM3MDAy.DoO71w.lhSrmW8bypZhqGQN9KyjKUh2PLI");
