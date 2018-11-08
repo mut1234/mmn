6de1ce60b554fa5ffe9693dac876c9d59fe0a729
mut1234 <44782684+mut1234@users.noreply.github.com>
2018-11-08 23:35:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,48 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "$";
-
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-var prefix = "$";
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ɢи cℓαи`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(` Coloring Team`,'https://www.twitch.tv/zya2d_')
+    client.user.setStatus('ldle');
 });
+var prefix = "-";
+ 
+client.on("message", message => {
+ 
+            if (message.content.startsWith(prefix + "obc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};    
+});
+ 
+ 
+ 
+ 
+ 
+ 
+client.on('message', message => {
+    if(message.content.startsWith('-sss')) {
+    let args = message.content.split(' ').slice(1);
+    let ar = args.join(' ');
+ 
+    message.channel.send(ar,{tts:true});
+}
+});
+ 
 
 
-
-client.login("NDczNjMwNTg5MDU3MjM3MDAy.DsYtDQ.MXQ4wtuVka2CKpTsNYWKPmWrn80");
 
 client.login(process.env.BOT_TOKEN);
