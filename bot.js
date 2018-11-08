51cc8b079dc1f39cd2e4f8930c86c15ed96c1a6b
mut1234 <44782684+mut1234@users.noreply.github.com>
2018-11-08 23:12:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,4 +17,16 @@ client.on("message", message => {
 });
 
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`ɢи cℓαи`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
 client.login(process.env.BOT_TOKEN);
